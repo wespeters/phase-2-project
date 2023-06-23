da5dddb529fc083c090d4be95b4c9223e5822557
Wesley Peters <wesleypeters@gmail.com>
2023-06-23 14:06:21 -0600

    debugging changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from './logo.svg';
-import { Route, Switch } from "react-router.dom";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <Switch>
-        <Route path ="/">
-          <Home>
-          </Home>
-        </Route>
-      </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
-export default App;
